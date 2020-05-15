829012cd7a122f45debd0a6e3dfee2eff16d6a2a
AlexMakdis <alexmakd@student.arteveldehs.be>
2020-05-16 00:22:54 +0200

    cursor

--- docs/data/threeSlider/index2.js
+++ docs/data/threeSlider/index2.js
@@ -388,17 +388,17 @@ function createTweenScrubber(tween, seekSpeed) {
 
   // desktop
   var mouseDown = false;
-  
- document.getElementById('three-container').style.cursor = 'pointer'
+  document.getElementById('three-container').style.cursor = 'pointer';
+
   window.addEventListener('mousedown', function(e) {
     mouseDown = true;
-    document.body.style.cursor = 'ew-resize';
+    document.getElementById('three-container').style.cursor = 'ew-resize';
     _cx = e.clientX;
     stop();
   });
   window.addEventListener('mouseup', function(e) {
     mouseDown = false;
-    document.body.style.cursor = 'default';
+    document.getElementById('three-container').style.cursor = 'pointer';
     resume();
   });
   window.addEventListener('mousemove', function(e) {
